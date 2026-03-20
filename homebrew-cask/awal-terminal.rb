fee66466a602ea0f5ff2a15a4ebbcf2653dead05
brahim-guaali <guaalibrahim@gmail.com>
2026-03-20 10:40:04 +0400

    Update Homebrew cask for v0.16.0

--- homebrew-cask/awal-terminal.rb
+++ homebrew-cask/awal-terminal.rb
@@ -1,6 +1,6 @@
 cask "awal-terminal" do
-  version "0.15.0"
-  sha256 "09be5a17af28945cc11660a2b387dc2a36753661b855f204fed901dcaf9a6f0b"
+  version "0.16.0"
+  sha256 "364af7fe2b3d8b43689435ba1cccc3d47568272f2fc617445460b04ffcf0525a"
 
   url "https://github.com/AwalTerminal/Awal-terminal/releases/download/v#{version}/AwalTerminal.zip"
   name "Awal Terminal"
